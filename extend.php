51d9ae1889211b74d2bcba6bb213ec86736e3b21
766ez <endhesspvp@gmail.com>
2025-02-10 17:30:08 +0100

    azer

--- extend.php
+++ extend.php
@@ -8,7 +8,7 @@ return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/less/forum.less')
-        ->routes('/test', 'test'),
+        ->routes('/test', __DIR__.'/js/src/forum/index.ts'),
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js')
         ->css(__DIR__.'/less/admin.less'),

add17c36685fb23d6ec4a88faa9b7f44d64b0842
766ez <endhesspvp@gmail.com>
2025-02-10 16:39:49 +0100

    aze

--- js/src/forum/components/TestComponent.js
+++ js/src/forum/components/TestComponent.js
@@ -2,6 +2,6 @@ import Page from "flarum/components/Page"
 
 export default class GroupListPage extends Page {
     view() {
-        return m('div', 'Hello World')
+        return <div>Hello from TestComponent!</div>;
     }
 }

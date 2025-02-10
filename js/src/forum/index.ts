762c57e3a02d7d2264fc80639c9011e96d7da559
766ez <endhesspvp@gmail.com>
2025-02-10 17:41:27 +0100

    aze

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,9 +1,20 @@
 import app from 'flarum/forum/app';
+import {extend} from 'flarum/extend';
+import UserControls from 'flarum/utils/UserControls';
+import Button from 'flarum/components/Button';
+
 import TestComponent from './components/TestComponent';
 
 export { default as extend } from './extend';
 
 app.initializers.add('766-test', () => {
+  extend(UserControls, "test", (items, user) => {
+    items.add('test-a', Button.component({
+      onclick() {
+        console.log("a")
+      }
+    }))
+  })
   console.log("a")
   app.routes.test = {path: '/test', component: TestComponent}
 

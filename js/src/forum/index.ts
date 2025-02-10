5f9f66d7429c5d361ecf5eb2416e0c158472c5bf
766ez <endhesspvp@gmail.com>
2025-02-10 18:43:06 +0100

    azeqsd

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -8,16 +8,6 @@ import TestComponent from './components/TestComponent';
 export { default as extend } from './extend';
 
 app.initializers.add('766-test', () => {
-  extend(UserControls, "test", (items, user) => {
-    items.add('test-a', Button.component({
-      children: app.translator.trans("test-birthday.forum.settings.link"),
-      onclick() {
-        console.log("a")
-      }
-    }))
-  })
-  console.log("a")
-  app.routes.test = {path: '/test', component: TestComponent}
 
   console.log('[766/test] Hello, forum!');
 });

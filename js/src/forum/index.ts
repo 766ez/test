2eaa20fa0710c6114282a7e27de719c66394ab2d
766ez <endhesspvp@gmail.com>
2025-02-10 16:25:38 +0100

    azeaze

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -5,9 +5,7 @@ export { default as extend } from './extend';
 
 app.initializers.add('766-test', () => {
   console.log("a")
-  app.routes["test"] = {
-    path: "/test",
-    component: TestComponent,
-  }
+  app.routes.test = {path: '/test', component: TestComponent}
+
   console.log('[766/test] Hello, forum!');
 });
